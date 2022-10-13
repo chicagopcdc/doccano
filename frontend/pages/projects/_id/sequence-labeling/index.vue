a48c24c4f0df65868ec42212558c7877aa24833d
Nicholas Ferraz <9021595+nikoferraz@users.noreply.github.com>
2022-10-13 10:50:56 -0500

    Removed remaining labels from the dropdown menu

--- frontend/pages/projects/_id/sequence-labeling/index.vue
+++ frontend/pages/projects/_id/sequence-labeling/index.vue
@@ -43,46 +43,38 @@
     <template #sidebar>
       <annotation-progress :progress="progress" />
       <!-- <v-card class="mt-4">
-        <v-card-title>
-          Label Types
-          <v-spacer />
-          <v-btn icon @click="showLabelTypes = !showLabelTypes">
-            <v-icon>{{ showLabelTypes ? mdiChevronUp : mdiChevronDown }}</v-icon>
-          </v-btn>
-        </v-card-title>
-        <v-expand-transition>
-          <v-card-text v-show="showLabelTypes">
-            <v-switch v-if="useRelationLabeling" v-model="relationMode">
-              <template #label>
-                <span v-if="relationMode">Relation</span>
-                <span v-else>Span</span>
-              </template>
-            </v-switch>
-            <v-chip-group v-model="selectedLabelIndex" column>
-              <v-chip
-                v-for="(item, index) in labelTypes"
-                :key="item.id"
-                v-shortkey="[item.suffixKey]"
-                :color="item.backgroundColor"
-                filter
-                :text-color="$contrastColor(item.backgroundColor)"
-                @shortkey="selectedLabelIndex = index"
+        <v-card-title>L=This needs to go</v-card-title>
+        <v-card-text>
+          <v-switch v-if="useRelationLabeling" v-model="relationMode">
+            <template #label>
+              <span v-if="relationMode">Relation</span>
+              <span v-else>Span</span>
+            </template>
+          </v-switch>
+          <v-chip-group v-model="selectedLabelIndex" column>
+            <v-chip
+              v-for="(item, index) in labelTypes"
+              :key="item.id"
+              v-shortkey="[item.suffixKey]"
+              :color="item.backgroundColor"
+              filter
+              :text-color="$contrastColor(item.backgroundColor)"
+              @shortkey="selectedLabelIndex = index"
+            >
+              {{ item.text }}
+              <v-avatar
+                v-if="item.suffixKey"
+                right
+                color="white"
+                class="black--text font-weight-bold"
               >
-                {{ item.text }}
-                <v-avatar
-                  v-if="item.suffixKey"
-                  right
-                  color="white"
-                  class="black--text font-weight-bold"
-                >
-                  {{ item.suffixKey }}
-                </v-avatar>
-              </v-chip>
-            </v-chip-group>
-          </v-card-text>
-        </v-expand-transition>
-      </v-card>
-      <list-metadata :metadata="doc.meta" class="mt-4" /> -->
+                {{ item.suffixKey }}
+              </v-avatar>
+            </v-chip>
+          </v-chip-group>
+        </v-card-text>
+      </v-card> -->
+      <!-- <list-metadata :metadata="doc.meta" class="mt-4" /> -->
     </template>
   </layout-text>
 </template>
