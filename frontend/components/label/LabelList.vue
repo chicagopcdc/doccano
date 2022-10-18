426f28badd89b60150e16d4a2ab89b67f6701843
Nicholas Ferraz <9021595+nikoferraz@users.noreply.github.com>
2022-10-18 14:30:51 -0500

    Added label description

--- frontend/components/label/LabelList.vue
+++ frontend/components/label/LabelList.vue
@@ -82,14 +82,16 @@ export default Vue.extend({
   computed: {
     headers() {
       const headers = [
-        { text: this.$t('generic.name'), value: 'text', sortable: true },
-        { text: this.$t('labels.shortkey'), value: 'suffixKey', sortable: true },
-        { text: this.$t('labels.color'), value: 'backgroundColor', sortable: true }
-      ]
+        { text: this.$t('generic.name'), value: 'text' },
+        { text: this.$t('labels.shortkey'), value: 'suffixKey' },
+        { text: this.$t('labels.color'), value: 'backgroundColor' },
+        { text: this.$t('labels.description'), value: 'description' },
+      ];
+
       if (!this.disableEdit) {
-        headers.push({ text: 'Actions', value: 'actions', sortable: false })
+        headers.push({ text: 'Actions', value: 'actions', sortable: false });
       }
-      return headers
+      return headers;
     }
   }
 })
