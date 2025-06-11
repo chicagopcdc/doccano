26d0902c2fb2891c27a122cddb5cb1da5e7a5d85
bashirmatov <bashirmatov@Mac.attlocal.net>
2025-06-11 16:57:31 -0500

    update module release version and Cert ARN

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -18,7 +18,7 @@ module "doccano_ecs" {
 }
 
 module "doccano_efs" {
-  source          = "git::ssh://git@github.com/chicagopcdc/terraform_modules.git//aws/efs?ref=0.4.2"
+  source          = "git::ssh://git@github.com/chicagopcdc/terraform_modules.git//aws/efs?ref=0.5.0"
   vpc_id          = var.vpc_id
   efs_name        = var.app_name
   subnet_ids      = var.private_subnet_ids
@@ -64,15 +64,16 @@ module "doccano_efs" {
 }
 
 module "doccano_alb" {
-  source      = "git::ssh://git@github.com/chicagopcdc/terraform_modules.git//aws/alb?ref=0.4.2"
+  source      = "git::ssh://git@github.com/chicagopcdc/terraform_modules.git//aws/alb?ref=0.5.0"
   environment = var.environment
   app_name    = "doccano"
   vpc_id      = var.vpc_id
   subnet_ids  = var.private_subnet_ids
+  acm_cert_arn =  "arn:aws:acm:us-east-1:471835990085:certificate/c1cebe37-738d-4766-a635-bc23d3e8caa7" # 
 }
 
 module "rds" {
-  source              = "git::ssh://git@github.com/chicagopcdc/terraform_modules.git//aws/rds?ref=0.4.2"
+  source              = "git::ssh://git@github.com/chicagopcdc/terraform_modules.git//aws/rds?ref=0.5.0"
   rds_security_groups = module.doccano_ecs.ecs_tasks_sg
   vpc_id              = var.vpc_id
   subnet_ids          = var.private_subnet_ids
